81fe961972f08cc1aa931124b92c1fefc3d32e93
groceryheist <nathante@uw.edu>
2019-04-18 14:46:58 -0700

    get user info from the api

--- bias_analysis/pull_training_data.py
+++ bias_analysis/pull_training_data.py
@@ -6,6 +6,13 @@ import shutil
 import mwapi
 import json
 from ores.utilities.score_revisions import run as call_ores
+from revscoring.extractors import api
+from revscoring.utilities.extract import extract, ConfiguredExtractor
+import yamlconf
+from revscoring.dependencies import Dependent
+import editquality.feature_lists
+from concurrent.futures import ThreadPoolExecutor
+import itertools
 
 def load_wikis():
     lines = open("rcfilters_enabled.csv",'r')
@@ -61,7 +68,7 @@ def load_labels(label_file):
 
 def score_labels(labels,context,label_file):
     ores_host = "https://ores.wikimedia.org/"
-    user_agent = "Ores bias analysis project by Nate TeBlunthuis (groceryheist)"
+    user_agent = "Ores bias analysis project by Nate TeBlunthuis <groceryheist@uw.edu>"
     if not os.path.exists("../datasets/scored_labels"):
         os.makedirs("../datasets/scored_labels")
     output_file = open("../datasets/scored_labels/{0}".format(os.path.split(label_file)[1]),'w')
@@ -69,12 +76,101 @@ def score_labels(labels,context,label_file):
               parallel_requests=4,retries=2,input=labels,output=output_file,batch_size=50,verbose=True)
     output_file.close()
 
+# get the features
+def extract_features(label_file,context):
+    rev_ids = [json.loads(label) for label in load_labels(label_file)]
+    
+    session = mwapi.Session(
+        host= "https://{0}.wikipedia.org".format(
+            context.replace("wiki","")),
+        user_agent="Ores bias analysis project by Nate TeBlunthuis <groceryheist@uw.edu>")
+
+    dependent_names = ["editquality.feature_lists.{0}.damaging".format(context),
+                  "editquality.feature_lists.{0}.goodfaith".format(context)]
+    dependents = []
+    for dependent_path in dependent_names:
+        dependent_or_list = yamlconf.import_path(dependent_path)
+        if isinstance(dependent_or_list, Dependent):
+            dependents.append(dependent_or_list)
+        else:
+            dependents.extend(dependent_or_list)
+
+    extractor = api.Extractor(session)
+    features = extract(dependents, rev_ids, extractor,extractors=os.cpu_count() - 1)
+    return features
+
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
+
+
+def get_editor_traits(labels, context, output):
+    rev_ids = [json.loads(label)['rev_id'] for label in labels]
+    # special case for wikidata
+
+    if context == "wikidatawiki":
+        host= "https://wikidata.org".format(context.replace("wiki",""))
+    else:
+        host= "https://{0}.wikipedia.org".format(context.replace("wiki",""))
+
+    user_agent="Ores bias analysis project by Nate TeBlunthuis <groceryheist@uw.edu>"
+    session = mwapi.Session(host,user_agent)
+
+    batches = grouper(rev_ids, 50)
+
+    def table_results(batch, context):
+        row = {}
+        resultset = batch['query']['pages']
+        for _, page_id in enumerate(resultset):
+            result = resultset[page_id]
+            row['ns'] = result['ns']
+            row['title'] = result['title']
+            row['pageid'] = result['pageid']
+            row['wiki'] = context
+            revisions = result['revisions']
+            for rev in revisions:
+                row['revid'] = rev['revid']
+                row['parentid'] = rev['parentid']
+                if 'user' in rev:
+                    row['user'] = rev['user']
+                    row['userid'] = rev['userid']
+                else:
+                    row['user'] = None
+                    row['userid'] = None
+                yield row
+            
+    with ThreadPoolExecutor(100) as executor:
+        # get revision metadata
+        revision_batches = executor.map(lambda batch:session.get(action="query", prop='revisions', rvprop=['ids','user','userid'], revids=batch, ), batches)
+
+        badrevids = []
+        rows = []
+        for batch in revision_batches:
+            if 'badrevids' in batch['query']:
+                badrevids.append(batch['query']['badrevids'])
+            for row in table_results(batch, context):
+                out_row = '\t'.join([str(row[key]) for key in out_schema])
+                output.write(out_row + '\n')
+
+
 if __name__ == "__main__":
     wikis = load_wikis()
     makefile = load_makefile()
     label_files = list(map(lambda x: grep_labelfile(x, makefile), wikis))
     list(map(download_labels,label_files))
     [score_labels(load_labels(label_file),wiki,label_file) for wiki,label_file in zip(wikis,label_files)]
+    
+    with open("editor_revisions.tsv",'w') as label_metadata_temp1:
+        out_schema = ['wiki','ns','pageid','title','revid','parentid','user','userid']
+
+        label_metadata_temp1.write('\t'.join(out_schema))
+
+        for label_file, context in zip(label_files,wikis):
+            labels = load_labels(label_file)
+            get_editor_traits(labels,context,label_metadata_temp1)
     
     
 # next step
