a8fd754201cbeeb1572ac9f5e52b6338c105c650
Nathan TeBlunthuis <nathante@uw.edu>
2019-06-04 17:02:11 +0000

    add and reorganize code for fitting models

--- bias_analysis/Ores_bias_causal.r
+++ bias_analysis/Ores_bias_causal.r
@@ -1,226 +1,102 @@
-
-install.packages("sandwich")
-
-library(MASS)
-library(aod)
+## this file has the frequentist analysis
 library(sandwich)
 library(sjPlot)
 library(lmtest)
-
-library(lmtest)
-
+library(MASS)
+library(aod)
+options(mc.cores = parallel::detectCores())
+set.seed(1337)
 source("ob_util.R")
+source("formulas.r")
 
 df <- load_ts_ds()
 
-set.seed(123)
+df <- prepare.df(df)
+wiki.stats <- prepare.wikistats(df)
 
-# gotta do a regex replace to fix some of the names
-names(df)[42:71] <- sapply(names(df)[42:71], function(n) paste0("ns",n))
+## drop 3 wikis that don't have any ns4 edits
+wiki.stats <- wiki.stats[ns4.edits != 0]
+df <- df[wiki.db %in% wiki.stats$wiki.db]
 
-df[,wiki_db := factor(wiki_db)]
+# the dv for model1 is ttr
+## scale everything
+## view count and active editors are super correlated with reverts
 
-cutoffs <- df[treated==T,cutoff]
-get_rand_cutoff <- function(wiki){
-    return(sample(cutoffs,1))
-}
+# 48 hours for non-reverted as a hueristic for "reverted_for_damage"
 
-df[treated == F, cutoff := get_rand_cutoff(wiki_db),by=.(wiki_db)]
+# add edits by bots to the treatment model
+# we don't include ns4.reverts be
 
-df[treated == F, weeks_from_cutoff := round((week - cutoff)/dweeks(1))]
+treated.model.1 = glm(data =wiki.stats, treatment.formula , family=binomial(link='logit'),na.action = na.fail)
 
-# there's a problem with n_pages_baseline
+## huston we have a problem with perfect seperation so we want to use the firth-penalized liklihood instead
 
-df[is.na(view_count),view_count := 0]
-df[is.na(patroller_N_reverts),patroller_N_reverts := 0]
-df[is.na(admin_N_reverts),admin_N_reverts := 0]
-df[is.na(bot_N_reverts),bot_N_reverts := 0]
-df[is.na(other_N_reverts),other_N_reverts := 0]
-df[is.na(anonymous_n_reverted),anonymous_n_reverted := 0]
-df[is.na(newcomer_n_reverted),newcomer_n_reverted := 0]
-df[is.na(established_n_reverted),established_n_reverted := 0]
-df[is.na(ns4_anon_N_edits),ns4_anon_N_edits := 0]
-df[is.na(ns4_newcomer_N_edits),ns4_newcomer_N_edits := 0]
-df[is.na(ns4_non_anon_newcomer_N_edits),ns4_non_anon_newcomer_N_edits := 0]
-df[is.na(ns0_anon_N_edits),ns0_anon_N_edits := 0]
-df[is.na(ns0_newcomer_N_edits),ns0_newcomer_N_edits := 0]
-df[is.na(ns0_non_anon_newcomer_N_edits),ns0_non_anon_newcomer_N_edits := 0]
-df[is.na(n_pages_baseline_ns_0),n_pages_baseline_ns_0 := 0]
-df[is.na(n_pages_baseline_ns_4),n_pages_baseline_ns_4 := 0]
-df[is.na(user_week_revert_cv),user_week_revert_cv := 0]
-df[is.na(geom_mean_ttr),geom_mean_ttr := 0]
+# turns out that logistf leads to smaller weights than the bayesian approach
+library(logistf)
+treated.model.1.1 = logistf(data =wiki.stats, formula=treatment.formula)
 
-wiki_stats = df[(weeks_from_cutoff > -52) & (week < cutoff),
-                .(treated = any(treated,na.rm=T),
-#                  reverts_per_week = mean(n_reverts,na.rm=T),
-                  user_revert_cv = mean(user_week_revert_cv,na.rm=T),
-#                  edits = mean(total_edits,na.rm=T),
-                  active_editors = mean(active_editors,na.rm=T),
-                  ttr = mean(geom_mean_ttr,na.rm=T),
-#                         admin_ttr = mean(admin_geom_mean_ttr,na.rm=T), 
-#                         bot_ttr = mean(bot_geom_mean_ttr,na.rm=T), 
-                  admin_reverts = mean(admin_N_reverts,na.rm=T), 
-                  bot_reverts = mean(bot_N_reverts,na.rm=T), #
-#                         patroller_ttr = mean(patroller_mean_ttr,na.rm=T),
-                  patroller_reverts = mean(patroller_N_reverts,na.rm=T),
-#                         other_ttr = mean(other_mean_ttr,na.rm=T), 
-                  other_reverts = mean(other_N_reverts,na.rm=T),
-                  newcomer_reverted = mean(newcomer_n_reverted,na.rm=T),
- #                        newcomer_ttr = mean(newcomer_mean_ttr,na.rm=T),
-                  anon_reverted = mean(anonymous_n_reverted,na.rm=T),
- #                        anon_ttr = mean(anonymous_mean_ttr,na.rm=T), 
- #                 established_reverted = mean(established_n_reverted,na.rm=T),
- #                        established_ttr = mean(established_mean_ttr,na.rm=T),
-                  ns0_anon_edits = mean(ns0_anon_N_edits,na.rm=T),
-      #                   ns0_anon_editors = mean(ns0_anon_N_editors,na.rm=T),
-                  ns0_newcomer_edits = mean(ns0_newcomer_N_edits,na.rm=T),
-     #                    ns0_newcomer_editors = mean(ns0_newcomer_N_editors,na.rm=T),
-                  ns0_established_edits = mean(ns0_non_anon_newcomer_N_edits,na.rm=T),
-    #                     ns0_established_editors = mean(ns0_non_anon_newcomer_N_editors,na.rm=T),
-    #                     ns1_anon_edits = mean(ns1_anon_N_edits,na.rm=T),
-   #                      ns1_anon_editors = mean(ns1_anon_N_editors,na.rm=T),
-   #                      ns1_newcomer_edits = mean(ns1_newcomer_N_edits,na.rm=T),
-  #                       ns1_newcomer_editors = mean(ns1_newcomer_N_editors,na.rm=T),
-  #                       ns1_established_edits = mean(ns1_non_anon_newcomer_N_edits,na.rm=T),
- #                        ns1_established_editors = mean(ns1_non_anon_newcomer_N_editors,na.rm=T),
- #                       ns2_anon_edits = mean(ns2_anon_N_edits,na.rm=T),
-#                         ns2_anon_editors = mean(ns2_anon_N_editors,na.rm=T),
- #                        ns2_newcomer_edits = mean(ns2_newcomer_N_edits,na.rm=T),
-#                         ns2_newcomer_editors = mean(ns2_newcomer_N_editors,na.rm=T),
- #                        ns2_established_edits = mean(ns2_non_anon_newcomer_N_edits,na.rm=T),
-#                         ns2_established_editors = mean(ns2_non_anon_newcomer_N_editors,na.rm=T),
- #                        ns3_anon_edits = mean(ns3_anon_N_edits,na.rm=T),
-#                         ns3_anon_editors = mean(ns3_anon_N_editors,na.rm=T),
-#                         ns3_newcomer_edits = mean(ns3_newcomer_N_edits,na.rm=T),
-#                         ns3_newcomer_editors = mean(ns3_newcomer_N_editors,na.rm=T),
-#                         ns3_established_edits = mean(ns3_non_anon_newcomer_N_edits,na.rm=T),
-#                         ns3_established_editors = mean(ns3_non_anon_newcomer_N_editors,na.rm=T),
-                  ns4_edits = mean(ns4_anon_N_edits + ns4_newcomer_N_edits + ns4_non_anon_newcomer_N_edits, na.rm=T),
-#                  ns4_anon_edits = mean(ns4_anon_N_edits,na.rm=T),
-#                         ns4_anon_editors = mean(ns4_anon_N_editors,na.rm=T),
-#                  ns4_nelwcomer_edits = mean(ns4_newcomer_N_edits,na.rm=T),
-#                         ns4_newcomer_editors = mean(ns4_newcomer_N_editors,na.rm=T),
-#                  ns4_established_edits = mean(ns4_non_anon_newcomer_N_edits,na.rm=T),
-#                         ns4_established_editors = mean(ns4_non_anon_newcomer_N_editors,na.rm=T),
-                  view_count = mean(view_count,na.rm=T)
-#                         n_pages_created_ns_0 =  mean(n_pages_created_ns_0,na.rm=T),
-#                         n_pages_created_ns_1 =  mean(n_pages_created_ns_1,na.rm=T),
-#                         n_pages_created_ns_2 = mean(n_pages_created_ns_2,na.rm=T),
-#                         n_pages_created_ns_3 = mean(n_pages_created_ns_3,na.rm=T),
-#                         n_pages_created_ns_4 = mean(n_pages_created_ns_4,na.rm=T),
-#                  n_pages_baseline_ns_0 = mean(n_pages_baseline_ns_0,na.rm=T),
-#                         n_pages_baseline_ns_1 = mean(n_pages_baseline_ns_1,na.rm=T),
-#                         n_pages_baseline_ns_2 = mean(n_pages_baseline_ns_2,na.rm=T),
-#                         n_pages_baseline_ns_3 = mean(n_pages_baseline_ns_3,na.rm=T),
-#                  n_pages_baseline_ns_4 = mean(n_pages_baseline_ns_4,na.rm=T)),
-                  ),
-                by=.(wiki_db)]
+coeftest(treated.model.1.1)
 
+wiki.stats[,treated.probs.pred := treated.model.1.1$predict]
+wiki.stats[treated == T,ip.weight := 1/treated.probs.pred]
+wiki.stats[treated == F,ip.weight := 1/(1-treated.probs.pred)]
 
-# for (name in names(wiki_stats)){
-#     if(is.numeric(wiki_stats[[name]])){
-#         wiki_stats[[name]] <- scale(wiki_stats[[name]])
-#     }
-# }
-
-wiki_stats
-
-treated_model = glm(data =wiki_stats, formula=treated ~ . - wiki_db, family=binomial(link='logit'),na.action = na.fail)
-
-summary(treated_model)
-
-wiki_stats[,treated_odds_pred := exp(predict(treated_model,wiki_stats))]
-wiki_stats[,treated_probs_pred := treated_odds_pred / (1+treated_odds_pred)]
-wiki_stats[treated == T,ip_weight := 1/treated_probs_pred]
-wiki_stats[treated == F,ip_weight := 1/(1-treated_probs_pred)]
-
-wiki_stats[treated_probs_pred > 0.10 | treated,
-           .(mean(treated_probs_pred),.N),by=.(treated)]
+wiki.stats[treated.probs.pred > 0.10 | treated,
+           .(mean(treated.probs.pred),.N),by=.(treated)]
 
 # add the IP-weights to the df
-df <- merge(df,wiki_stats,by=c("wiki_db"),how='left outer',suffixes=c('','.y'))
+
+df <- merge(df,wiki.stats,by=c("wiki.db"),how='left outer',suffixes=c('','.y'))
 
 # let's check if excluding the wikis with the fewest reverts changes things
 # df = df[N_reverts >= median(N_reverts)]
 
 #for this test we'll use random cutoffs for each wiki 
 
-df <- df[order(wiki_db,week)]
-
-df[,":="(geom_mean_ttr_l1 = shift(geom_mean_ttr,type = 'lag',n=1),
-        geom_mean_ttr_l2 = shift(geom_mean_ttr,type = 'lag',n=2),
-        geom_mean_ttr_l3 = shift(geom_mean_ttr,type = 'lag',n=3),
-        geom_mean_ttr_l4 = shift(geom_mean_ttr,type = 'lag',n=4))]
-
-df[,":="(geom_mean_ttr_l1pl2 = geom_mean_ttr_l1 + geom_mean_ttr_l2)]
-
-df[,":="(week_factor = factor(week))]
-
-mod_treatment_test <- glm(df[(weeks_from_cutoff ==0)] ,formula = "treated ~ geom_mean_ttr_l1 + geom_mean_ttr_l1pl2", family=binomial(link='logit'))
-
-summary(mod_treatment_test)
-
-names(df[(weeks_from_cutoff==0)])
-
-df <- df[,geom_mean_ttr_demeaned := geom_mean_ttr - mean(.SD[['geom_mean_ttr']]), by=.(wiki_db)]
-df <- df[,weeks_from_cutoff_sq := weeks_from_cutoff^2, by=.(wiki_db)]
-df <- df[,N_reverts_demeaned := N_revert - mean(.SD[["N_revert"]]), by=.(wiki_db)]
-df <- df[,ineq_demeaned := user_week_revert_cv - mean(user_week_revert_cv), by=.(wiki_db)]
-df <- df[,hhi_demeaned := revert_hhi - mean(revert_hhi), by=.(wiki_db)]
-df <- df[,treated_with_ores := treated & has_ores]
-
-names(df)
-
 # we have clustered errors
 # H1 geom_mean_ttr
-# DID only 
-mod1_did <- lm(df,formula="geom_mean_ttr ~ 1 + treated + week_factor + treated_with_ores")
 
-vcov_did <- sandwich::vcovCL(mod1_did,df$wiki_db)
+##DID only 
 
+mod1.did <- lm(m1.formula,df)
 
-coeftest(mod1_did,vcov_did)
+vcov.did <- sandwich::vcovCL(mod1.did,df$wiki.db)
 
-#with IP weights
-mod1_ip <- lm(df,formula=as.formula("geom_mean_ttr ~ 1 + treated + week_factor + treated_with_ores"), weights = df$ip_weight)
+coeftest(mod1.did,vcov.did)
 
-vcov_ip <- sandwich::vcovCL(mod1_ip,df$wiki_db)
+##with IP weights
+mod1.ip <- lm(df,formula= m1.formula, weights = df$ip.weight)
 
-coeftest(mod1_ip,vcov_ip)
+vcov.ip <- sandwich::vcovCL(mod1.ip,df$wiki.db)
+
+coeftest(mod1.ip,vcov.ip)
 
 #doubly robust 
-mod1_dr <- lm(df, formula=as.formula("geom_mean_ttr ~ 1 + treated + week_factor + treated_with_ores + user_revert_cv + active_editors + admin_reverts + bot_reverts + patroller_reverts + other_reverts + newcomer_reverted + anon_reverted + ns0_anon_edits + ns0_newcomer_edits + ns0_established_edits + ns4_edits + view_count"), weights=df$ip_weight)
+mod1.dr <- lm(df, formula= m1.formula.dr, weights=df$ip.weight)
 
-vcov_dr <- sandwich::vcovCL(mod1_dr,df$wiki_db)
+vcov.dr <- sandwich::vcovCL(mod1.dr,df$wiki.db)
 
-coeftest(mod1_dr,vcov_dr)
+coeftest(mod1.dr,vcov.dr)
 
-summary(mod1_ip)
-
-summary(mod1_dr)
-
-mod1 <- lm(df,formula="geom_mean_ttr_demeaned ~ 1 + treated + week_factor + treated_with_ores")
-
-summary(mod1)
-
-df <- df[,mod1_pred := predict(mod1,df)]
-
-qplot(log1p(df$N_revert),bins=80)
-
-df2 <- df
-
-library(aod)
-
-# H1a number of reverts
-# DID only
-mod2_did <- negbin(data=df, formula="N_revert ~ 1 + treated + week_factor + treated_with_ores",random=~ 1)
+mod2_did <- glm.nb(data=df, formula=m2.formula)
+ 
 saveRDS(object=mod2_did,file = "ores_bias_data/mod2_did.RDS")
 
-mod2_ip <- negbin(data=df, formula="N_revert ~ 1 + treated + week_factor + treated_with_ores", random=~1,weights=df2$ip_weight)
+vcov.did <- sandwich::vcovCL(mod2_did,df$wiki.db)
+
+coeftest(mod2_did,vcov.did)
+
+mod2_ip <- glm.nb(data=df, formula=m2.formula, weights=df$ip_weight)
 saveRDS(object=mod2_ip,file = "ores_bias_data/mod2_ip.RDS")
 
+vcov_ip <- sandwich::vcovCL(mod2_ip,df$wiki.db)
+
+coeftest(mod2_ip,vcov.did)
+
 # doubly robust
-mod2_dr <- negbin(data=df, formula="N_revert ~ 1 + treated + week_factor + treated_with_ores + user_week_revert_cv + geom_mean_ttr + active_editors + ns0_anon_edits + ns0_newcomer_edits + ns0_established_edits + ns4_edits + view_count + geom_mean_ttr", random=~1,weights = df2$ip_weight)
+mod2_dr_pois <- glm(data=df,formula=m2.formula.dr, weights = df$ip_weight, family=poisson())
+
+mod2_dr <- glm.nb(data=df, formula=m2.formula.dr, weights = df$ip_weight, start=coef(mod2_dr_pois))
 saveRDS(object=mod2_dr,file = "ores_bias_data/mod2_dr.RDS")
 
 summary(mod2_did)
@@ -246,27 +122,63 @@ qplot(log(df$revert_hhi))
 df[user_week_revert_cv == 0,.(wiki_db,any(treated), mean(N_reverts)),by='wiki_db']
 
 # DID onlyh
-mod3_did <- lm(df[user_week_revert_cv != 0], formula="log(user_week_revert_cv) ~ 1 + treated + week_factor + treated_with_ores")
+# we should be using a zero-inflated model here
+library(crch)
+df2 <- df[user.week.revert.cv != 0]
+mod3.did <- glm(df, formula=m3.formula,family=Gamma())
 
-vcov_did <- sandwich::vcovCL(mod3_did,df[user_week_revert_cv!=0]$wiki_db)
+vcov.did <- sandwich::vcovCL(mod3.did,df[user.week.revert.cv!=0]$wiki.db)
 
-coeftest(mod3_did,vcov_did)
+coeftest(mod3.did,vcov.did)
 
 # IP weights
-df2 <- df[user_week_revert_cv != 0]
+mod3.ip <- glm(df2, formula=update(mod3.lhs,mod3.rhs), weights = df2$ip.weight, family=gaussian(link='log'))
 
-mod3_ip <- lm(df[user_week_revert_cv != 0], formula="log(user_week_revert_cv) ~ 1 + treated + week_factor + treated_with_ores", weights = df2$ip_weight)
+vcov.did <- sandwich::vcovCL(mod3.ip,df[user.week.revert.cv!=0]$wiki.db)
 
-vcov_did <- sandwich::vcovCL(mod3_ip,df[user_week_revert_cv!=0]$wiki_db)
+coeftest(mod3.ip,vcov.did)
 
-coeftest(mod3_ip,vcov_did)
+# doubly robust
+mod3.dr <- glm(df2, formula=update(mod3.lhs, paste(". ~ + ", paste0(treatment.rhs[3],"+"),paste0(mod3.rhs[3],"- user.revert.cv",collapse="+"))), weights = df2$ip.weight, family=gaussian(link='log'))
 
-mod3_dr <- lm(df[user_week_revert_cv != 0], formula="log(user_week_revert_cv) ~ 1 + treated + week_factor + treated_with_ores + geom_mean_ttr + active_editors + admin_reverts + geom_mean_ttr + newcomer_reverted + anon_reverted + ns0_anon_edits + ns0_newcomer_edits + ns0_established_edits + ns4_edits + view_count + admin_reverts + bot_reverts + patroller_reverts + other_reverts", weights = df2$ip_weight)
+vcov.dr <- sandwich::vcovCL(mod3.dr,df$wiki.db)
 
-vcov_dr <- sandwich::vcovCL(mod3_dr,df[user_week_revert_cv!=0]$wiki_db)
+coeftest(mod3.dr,vcov.dr)
 
-coeftest(mod3_dr,vcov_dr)
+mod4.did <- glm.nb(df,formula=m4.formula)
 
+vcov.did <- sandwich::vcovCL(mod4.did,df$wiki.db)
+
+coeftest(mod4.did,vcov.did)
+
+mod4.ip <- glm.nb(df,formula=m4.formula, weights=df$ip.weight)
+
+vcov.ip <- sandwich::vcovCL(mod4.ip,df$wiki.db)
+
+coeftest(mod4.ip,vcov.did)
+
+
+mod4.dr <- glm.nb(df, formula=m4.formula.dr, weights=df$ip.weight)
+
+mod5.did <- glm.nb(df,formula=m5.formula)
+
+vcov.ip <- sandwich::vcovCL(mod5.ip,df$wiki.db)
+
+coeftest(mod5.ip,vcov.ip)
+
+mod5.did <- glm.nb(df,formula=m4.formula)
+
+vcov.did <- sandwich::vcovCL(mod5.did,df$wiki.db)
+
+coeftest(mod5.did,vcov.did)
+
+mod5.ip <- glm.nb(df,formula=m5.formula, weights=df$ip.weight)
+
+vcov.ip <- sandwich::vcovCL(mod5.ip,df$wiki.db)
+
+coeftest(mod5.ip,vcov.ip)
+
+mod5.dr <- glm.nb(df, formula=m5.formula.dr, weights=df$ip.weight)
 
 qplot(log(1+df$user_week_revert_cv))
 
@@ -285,7 +197,6 @@ vcov_did <- sandwich::vcovCL(mod3_did,df[user_week_revert_cv!=0]$wiki_db)
 coeftest(mod3_did,vcov_did)
 
 ggplot(df,aes(x=weeks_from_cutoff)) + geom_point(aes(y=ineq_demeaned,color='data')) + geom_point(data=df,aes(y=mod3.pred,color='predicted')) + facet_wrap(.~never_treated) + geom_vline(data=df,xintercept=0)
-
 
 
 mod4 <- lm(df, formula="hhi_demeaned ~ has_ores never_treated*week_factor")
--- bias_analysis/ob_util.R
+++ bias_analysis/ob_util.R
@@ -6,7 +6,7 @@ library("smooth")
 library("lme4")
 theme_set(theme_bw())
 
-load_ts_ds = function(){
+load_ts_ds <- function(){
 
     treated = fread("ores_bias_data/rcfilters_enabled.csv")
     treated <- treated[,.(cutoff=min(timestamp)),by='Wiki']
@@ -20,5 +20,172 @@ load_ts_ds = function(){
     df[,has_ores:=weeks_from_cutoff >=0]
     df[is.na(weeks_from_cutoff),has_ores := FALSE]
     df[,treated := !all(is.na(weeks_from_cutoff)),by=.(wiki_db)]
+    names(df) <- sapply(names(df),function(name) gsub("_",".",name))
     return(df)
+}
+
+prepare.df <- function(df){
+
+    ## gotta do a regex replace to fix some of the names
+    names(df)[42:71] <- sapply(names(df)[42:71], function(n) paste0("ns",n))
+
+    df[,wiki.db := factor(wiki.db)]
+    df[,week.factor := factor(week)]
+
+    cutoffs <- df[treated==T,cutoff]
+    get.rand.cutoff <- function(wiki){
+        return(sample(cutoffs,1))
+    }
+
+    df[treated == F, cutoff := get.rand.cutoff(wiki.db),by=.(wiki.db)]
+
+    df[treated == F, weeks.from.cutoff := round((week - cutoff)/dweeks(1))]
+
+    ## there's a problem with n.pages.baseline
+
+    df[is.na(view.count),view.count := 0]
+    df[is.na(patroller.N.reverts),patroller.N.reverts := 0]
+    df[is.na(admin.N.reverts),admin.N.reverts := 0]
+    df[is.na(bot.N.reverts),bot.N.reverts := 0]
+    df[is.na(other.N.reverts),other.N.reverts := 0]
+    df[is.na(anonymous.n.reverted),anonymous.n.reverted := 0]
+    df[is.na(newcomer.n.reverted),newcomer.n.reverted := 0]
+    df[is.na(established.n.reverted),established.n.reverted := 0]
+    df[is.na(ns4.anon.N.edits),ns4.anon.N.edits := 0]
+    df[is.na(ns4.newcomer.N.edits),ns4.newcomer.N.edits := 0]
+    df[is.na(ns4.non.anon.newcomer.N.edits),ns4.non.anon.newcomer.N.edits := 0]
+    df[is.na(ns0.anon.N.edits),ns0.anon.N.edits := 0]
+    df[is.na(ns0.newcomer.N.edits),ns0.newcomer.N.edits := 0]
+    df[is.na(ns0.non.anon.newcomer.N.edits),ns0.non.anon.newcomer.N.edits := 0]
+    df[is.na(n.pages.baseline.ns.0),n.pages.baseline.ns.0 := 0]
+    df[is.na(n.pages.baseline.ns.4),n.pages.baseline.ns.4 := 0]
+    df[is.na(user.week.revert.cv),user.week.revert.cv := 0]
+    df[is.na(admin.geom.mean.ttr),admin.geom.mean.ttr := 0]
+    df[is.na(bot.geom.mean.ttr),bot.geom.mean.ttr := 0]
+    df[is.na(patroller.geom.mean.ttr),patroller.geom.mean.ttr := 0]
+    df[is.na(other.geom.mean.ttr),other.geom.mean.ttr := 0]
+
+    df[is.na(established.mean.ttr),established.mean.ttr := 0]
+    df[is.na(newcomer.mean.ttr),newcomer.mean.ttr := 0]
+    df[is.na(anonymous.mean.ttr),anonymous.mean.ttr := 0]
+
+    df[is.na(n.pages.created.ns.4),n.pages.created.ns.4 := 0]
+
+    df[,weekly.ns4.edits := ns4.anon.N.edits + ns4.newcomer.N.edits + ns4.non.anon.newcomer.N.edits]
+    df[,":="(p.ns4.edits.anon = ns4.anon.N.edits / weekly.ns4.edits,
+             p.ns4.edits.newcomer = ns4.newcomer.N.edits / weekly.ns4.edits,
+             p.ns4.edits.established = ns4.non.anon.newcomer.N.edits / weekly.ns4.edits)]
+
+    df[,ns0.edits := ns0.anon.N.edits + ns0.newcomer.N.edits + ns0.non.anon.newcomer.N.edits]
+    df[,":="(p.ns0.edits.anon = ns0.anon.N.edits / ns0.edits,
+             p.ns0.edits.newcomer = ns0.newcomer.N.edits / ns0.edits,
+             p.ns0.edits.established = ns0.non.anon.newcomer.N.edits / ns0.edits)]
+
+    df[, n.reverts := bot.N.reverts + admin.N.reverts + patroller.N.reverts + other.N.reverts]
+
+    df[,":="(p.reverts.bot = bot.N.reverts / n.reverts,
+             p.reverts.admin = admin.N.reverts / n.reverts,
+             p.reverts.patroller = patroller.N.reverts / n.reverts,
+             p.reverts.other = other.N.reverts / n.reverts )]
+
+    df[, n.reverteds := newcomer.n.reverted + anonymous.n.reverted + established.n.reverted]
+
+    df[,":="(p.reverteds.newcomer = newcomer.n.reverted / n.reverteds,
+             p.reverteds.anonymous = anonymous.n.reverted / n.reverteds,
+             p.reverteds.established = established.n.reverted / n.reverteds)]
+
+    df[, has.patrollers := patroller.N.reverts > 0]
+    df <- df[,treated.with.ores := treated & has.ores]
+
+    ## only look at 52 weeks prior to the change
+    df <- df[weeks.from.cutoff > -52]s
+    return(df)
+}
+
+prepare.wikistats <- function(df){
+    wiki.stats  <-  df[(weeks.from.cutoff > -52) & (week < cutoff),
+                       .(treated = any(treated,na.rm=T),
+                         has.patrollers = any(has.patrollers, na.rm=T),
+                         reverts.per.week = mean(n.reverts,na.rm=T),
+                         user.revert.cv = mean(user.week.revert.cv,na.rm=T),
+                         edits = mean(total.edits,na.rm=T),
+                         active.editors = mean(active.editors,na.rm=T),
+                         ttr = mean(geom.mean.ttr,na.rm=T),
+                         admin.ttr = mean(admin.geom.mean.ttr,na.rm=T), 
+                         bot.ttr = mean(bot.geom.mean.ttr,na.rm=T), 
+                         admin.reverts = mean(admin.N.reverts,na.rm=T), 
+                         bot.reverts = mean(bot.N.reverts,na.rm=T), #
+                         patroller.ttr = mean(patroller.geom.mean.ttr,na.rm=T),
+                         patroller.reverts = mean(patroller.N.reverts,na.rm=T),
+                         other.ttr = mean(other.geom.mean.ttr,na.rm=T), 
+                         other.reverts = mean(other.N.reverts,na.rm=T),
+                         newcomer.reverted = mean(newcomer.n.reverted,na.rm=T),
+                         newcomer.ttr = mean(newcomer.mean.ttr,na.rm=T),
+                         anon.reverted = mean(anonymous.n.reverted,na.rm=T),
+                         anon.ttr = mean(anonymous.mean.ttr,na.rm=T), 
+                         established.reverted = mean(established.n.reverted,na.rm=T),
+                         established.ttr = mean(established.mean.ttr,na.rm=T),
+                         n.reverteds = mean(n.reverteds, na.rm=T),
+                         p.reverteds.newcomer = mean(p.reverteds.newcomer, na.rm=T),
+                         p.reverteds.anonymous = mean(p.reverteds.anonymous, na.rm=T),
+                         p.reverteds.established = mean(p.reverteds.established, na.rm=T),
+                         ns0.anon.edits = mean(ns0.anon.N.edits,na.rm=T),
+                         ns0.anon.editors = mean(ns0.anon.N.editors,na.rm=T),
+                         ns0.newcomer.edits = mean(ns0.newcomer.N.edits,na.rm=T),
+                         ns0.newcomer.editors = mean(ns0.newcomer.N.editors,na.rm=T),
+                         ns0.established.edits = mean(ns0.non.anon.newcomer.N.edits,na.rm=T),
+                         ns0.established.editors = mean(ns0.non.anon.newcomer.N.editors,na.rm=T),
+                         ns1.anon.edits = mean(ns1.anon.N.edits,na.rm=T),
+                         ns1.anon.editors = mean(ns1.anon.N.editors,na.rm=T),
+                         ns1.newcomer.edits = mean(ns1.newcomer.N.edits,na.rm=T),
+                         ns1.newcomer.editors = mean(ns1.newcomer.N.editors,na.rm=T),
+                         ns1.established.edits = mean(ns1.non.anon.newcomer.N.edits,na.rm=T),
+                         ns1.established.editors = mean(ns1.non.anon.newcomer.N.editors,na.rm=T),
+                         ns2.anon.edits = mean(ns2.anon.N.edits,na.rm=T),
+                         ns2.anon.editors = mean(ns2.anon.N.editors,na.rm=T),
+                         ns2.newcomer.edits = mean(ns2.newcomer.N.edits,na.rm=T),
+                         ns2.newcomer.editors = mean(ns2.newcomer.N.editors,na.rm=T),
+                         ns2.established.edits = mean(ns2.non.anon.newcomer.N.edits,na.rm=T),
+                         ns2.established.editors = mean(ns2.non.anon.newcomer.N.editors,na.rm=T),
+                         ns3.anon.edits = mean(ns3.anon.N.edits,na.rm=T),
+                         ns3.anon.editors = mean(ns3.anon.N.editors,na.rm=T),
+                         ns3.newcomer.edits = mean(ns3.newcomer.N.edits,na.rm=T),
+                         ns3.newcomer.editors = mean(ns3.newcomer.N.editors,na.rm=T),
+                         ns3.established.edits = mean(ns3.non.anon.newcomer.N.edits,na.rm=T),
+                         ns3.established.editors = mean(ns3.non.anon.newcomer.N.editors,na.rm=T),
+                         ns4.edits = mean(weekly.ns4.edits, na.rm=T),
+                         ns4.anon.edits = mean(ns4.anon.N.edits,na.rm=T),
+                         ns4.anon.editors = mean(ns4.anon.N.editors,na.rm=T),
+                         ns4.newcomer.edits = mean(ns4.newcomer.N.edits,na.rm=T),
+                         ns4.newcomer.editors = mean(ns4.newcomer.N.editors,na.rm=T),
+                         ns4.established.edits = mean(ns4.non.anon.newcomer.N.edits,na.rm=T),
+                         ns4.established.editors = mean(ns4.non.anon.newcomer.N.editors,na.rm=T),
+                         p.ns4.edits.anon = mean(p.ns4.edits.anon, na.rm=T),
+                         p.ns4.edits.newcomer = mean(p.ns4.edits.newcomer, na.rm=T),
+                         p.ns4.edits.established = mean(p.ns4.edits.established, na.rm=T),
+                         ns0.edits = mean(ns0.edits,na.rm=T),
+                         p.ns0.edits.anon = mean(p.ns0.edits.anon, na.rm=T),
+                         p.ns0.edits.newcomer = mean(p.ns0.edits.newcomer, na.rm=T),
+                         p.ns0.edits.established = mean(p.ns0.edits.established, na.rm=T),
+                         n.reverts = mean(n.reverts, na.rm=T),
+                         p.reverts.bot = mean(p.reverts.bot, na.rm=T), 
+                         p.reverts.admin =  mean(p.reverts.admin, na.rm=T), 
+                         p.reverts.patroller =  mean(p.reverts.patroller, na.rm=T),
+                         p.reverts.other =  mean(p.reverts.other, na.rm=T),
+                         view.count = mean(view.count,na.rm=T),
+                         n.pages.created.ns.0 =  mean(n.pages.created.ns.0,na.rm=T),
+                         n.pages.created.ns.1 =  mean(n.pages.created.ns.1,na.rm=T),
+                         n.pages.created.ns.2 = mean(n.pages.created.ns.2,na.rm=T),
+                         n.pages.created.ns.3 = mean(n.pages.created.ns.3,na.rm=T),
+                         n.pages.created.ns.4 = mean(n.pages.created.ns.4,na.rm=T),
+                         n.pages.baseline.ns.0 = mean(n.pages.baseline.ns.0,na.rm=T),
+                         n.pages.baseline.ns.1 = mean(n.pages.baseline.ns.1,na.rm=T),
+                         n.pages.baseline.ns.2 = mean(n.pages.baseline.ns.2,na.rm=T),
+                         n.pages.baseline.ns.3 = mean(n.pages.baseline.ns.3,na.rm=T),
+                         n.pages.baseline.ns.4 = mean(n.pages.baseline.ns.4,na.rm=T)
+                         ),
+                       by=.(wiki.db)]
+
+
+    return(wiki.stats)
 }
